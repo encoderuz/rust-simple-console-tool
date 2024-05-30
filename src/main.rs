0b3961330fa0a71e02bc6a7272ba000a8ae63347
encoderuz <abduqodirxojiyev@gmail.com>
2024-05-30 21:29:24 +0500

    committed from local - activity purpose

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
